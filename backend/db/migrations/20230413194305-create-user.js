e27aef3f7da60cab5554e514d8ea59f3355b3014
Elizabeth Lowry <eglowry@gmail.com>
2023-04-17 14:38:39 -0600

    set up migration files

--- backend/db/migrations/20230413194305-create-user.js
+++ backend/db/migrations/20230413194305-create-user.js
@@ -14,6 +14,12 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      firstname: {
+        type: Sequelize.STRING(50)
+      },
+      lastName: {
+        type: Sequelize.STRING(50)
+      },
       username: {
         type: Sequelize.STRING(30),
         allowNull: false,
