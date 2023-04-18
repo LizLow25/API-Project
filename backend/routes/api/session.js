ad633da3bef71ca6243dc85b70b7896f73d21ebd
Elizabeth Lowry <eglowry@gmail.com>
2023-04-18 17:14:25 -0600

    get all spots completed

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
-const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const { check } = require('express-validator');
@@ -69,7 +69,7 @@ router.delete(
 );
 
 router.get(
-    '/',
+    '/', requireAuth,
     (req, res) => {
         const { user } = req;
         if (user) {
