f048837ffab9d793d6f66fb158f6668deae17c9c
Elizabeth Lowry <eglowry@gmail.com>
2023-05-06 12:14:34 -0600

    created modals

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
+import { Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
-
 
 function App() {
   const dispatch = useDispatch();
@@ -13,20 +11,11 @@ function App() {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
-
-
   return (
     <>
       <Navigation isLoaded={isLoaded} />
-      {isLoaded && (
-      <Switch>
-        <Route path="/signup">
-          <SignupFormPage />
-        </Route>
-      </Switch>
-      )}
+      {isLoaded && <Switch></Switch>}
     </>
-
   );
 }
 
