4d4a63f835c4dd62ab69f9c282448bc4bb1c40d7
Elizabeth Lowry <eglowry@gmail.com>
2023-05-08 21:53:03 -0600

    updated seeder files

--- backend/db/seeders/20230417224107-add-spots.js
+++ backend/db/seeders/20230417224107-add-spots.js
@@ -23,39 +23,99 @@ module.exports = {
       {
         ownerId: 1,
         address: '50308 Pura Vida Lane',
-        city: 'Playa Flamingo',
+        city: 'Tierras Morenas',
         state: 'Guanacaste',
         country: 'Costa Rica',
         lat: 10.4317,
         lng: 85.7846,
-        name: 'Costa Rica Beachfront Van',
-        description: 'Experience pura vida on the shores of the Pacific ocean.',
+        name: 'Costa Rican Mountain Retreat',
+        description: 'Experience pura vida in the Costa Rican mountains.',
         price: 20.99
       },
       {
         ownerId: 1,
-        address: '472 Snowshoe Lane',
+        address: '472 Prairiedog Road',
         city: 'Moose',
         state: 'WY',
         country: 'USA',
         lat: 43.6558,
         lng: 110.7183,
-        name: 'Vanlife in the Grand Tetons',
+        name: 'Partially Restored 1920s Sheep Wagon',
         description: 'Rent your mobile dreamhome and go offroad in the wilds of Wyoming.',
         price: 70.99
       },
       {
         ownerId: 2,
-        address: '275 Marionberry Lane',
-        city: 'Forks',
+        address: '275 Marionberry Street',
+        city: 'Asheville',
+        state: 'NC',
+        country: 'USA',
+        lat: 35.5951,
+        lng: 82.5515,
+        name: "Hippie Retreat in the NC Moutains",
+        description: 'Come read colorful books with your hip friends.',
+        price: 40.99
+      },
+      {
+        ownerId: 2,
+        address: '3456 Cacti Lane',
+        city: 'Armagosa Valley',
+        state: 'NV',
+        country: 'USA',
+        lat: 36.6438,
+        lng: 116.4003,
+        name: "Glamping Near Death Valley",
+        description: 'Star watch and listen to the silence in the Nevada desert.',
+        price: 80.50
+      },
+      {
+        ownerId: 1,
+        address: '8238 Pelican Place',
+        city: 'San Diego',
+        state: 'CA',
+        country: 'USA',
+        lat: 32.7157,
+        lng: 117.1611,
+        name: 'Beachside Airstream Retreat',
+        description: 'Live the beach life in sunny San Diego.',
+        price: 212
+      },
+      {
+        ownerId: 1,
+        address: '5060 Cornflower Circle',
+        city: 'Silver Creek',
         state: 'WA',
         country: 'USA',
-        lat: 47.9504,
-        lng: 124.3855,
-        name: "Vanlife on Washington's Olympic Peninsula",
-        description: 'Explore the mysteries and wonder of a temperate rainforest.',
-        price: 40.99
-      }
+        lat: 46.5254,
+        lng: 122.5904,
+        name: 'VanLife on Mayfield Lake',
+        description: 'Get closer to nature on a nine acre private retreat.',
+        price: 175
+      },
+      {
+        ownerId: 1,
+        address: '745 Callisto Lane',
+        city: 'Williams',
+        state: 'AZ',
+        country: 'USA',
+        lat: 35.2495,
+        lng: 112.1910,
+        name: 'Stargazing in the Desert',
+        description: 'This special retreat offers a radiotelescope and free gummy bears.',
+        price: 250
+      },
+      {
+        ownerId: 1,
+        address: '745 Rapids Road',
+        city: 'Twin Falls',
+        state: 'ID',
+        country: 'USA',
+        lat: 42.5558,
+        lng: 114.4701,
+        name: 'Funky Retreat Under Idaho Skies',
+        description: 'Enjoy glamping near a college town.',
+        price: 165
+      },
     ], {})
 
 
--- backend/db/seeders/20230417225410-add-spot-images.js
+++ backend/db/seeders/20230417225410-add-spot-images.js
@@ -23,47 +23,202 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         spotId: 1,
-        url: 'https://revelgear.com/wp-content/uploads/2020/02/VanLifeAnn_cover_mercedes_revelgear_costa_rica_vanlife.jpg',
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-47160631/original/36d3a1ae-5760-45c7-83c4-dfae0e01963c.jpeg?im_w=1200',
         preview: true,
       },
       {
         spotId: 1,
-        url: 'https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/39/90/ac.jpg',
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-47160631/original/38616ea5-4917-4e1a-8202-b23f9aa0abd3.jpeg?im_w=720',
         preview: false,
       },
       {
         spotId: 1,
-        url: 'https://media.istockphoto.com/id/1216218328/photo/view-from-bed.jpg?s=612x612&w=0&k=20&c=1_F_dePa9bhFbt2A1_lmE3ry0mSJr4YA8vVFDN0jh_8=',
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-47160631/original/9f83ed26-b146-4fb5-a2af-3d5e406331da.jpeg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 1,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-47160631/original/301618f2-ee18-47cb-b7c3-8af302e30198.jpeg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 1,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-47160631/original/301618f2-ee18-47cb-b7c3-8af302e30198.jpeg?im_w=720',
         preview: false,
       },
       {
         spotId: 2,
-        url: 'https://i0.wp.com/brookebeyond.com/wp-content/uploads/2020/05/P9242440-1024x768.jpg?ssl=1',
+        url: 'https://a0.muscache.com/im/pictures/20888646/a28f189d_original.jpg?im_w=1200',
         preview: true,
       },
       {
         spotId: 2,
-        url: 'https://socalvanlife.com/wp-content/uploads/2022/08/Teton-Van-Pic.jpg',
+        url: 'https://a0.muscache.com/im/pictures/d32e9321-dad0-42f8-abd1-49ef56d39936.jpg?im_w=1200',
         preview: false,
       },
       {
         spotId: 2,
-        url: 'https://i.pinimg.com/originals/49/f3/99/49f399a5e6157633f27d74dfb017938d.jpg',
+        url: 'https://a0.muscache.com/im/pictures/66997309/578244d9_original.jpg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 2,
+        url: 'https://a0.muscache.com/im/pictures/5d6c1726-205f-4367-b7fe-9432691b32dc.jpg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 2,
+        url: 'https://a0.muscache.com/im/pictures/20894636/e5584b39_original.jpg?im_w=720',
         preview: false,
       },
       {
         spotId: 3,
-        url: 'https://images.squarespace-cdn.com/content/v1/5626d3a1e4b0fe0e8117d7ac/1614888194492-20EH5EPEF60YKRXXLKDM/IMG_5366.jpg',
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-20812370/original/ce34e54d-2085-4dd8-835c-2fb4b453b88d.jpeg?im_w=1200',
         preview: true,
       },
       {
         spotId: 3,
-        url: 'https://images.squarespace-cdn.com/content/v1/5f3c7f8ab0983c4328c3399d/1636493690570-EQ8ZX8EH3H9WLJM0ROOZ/IMG_3115+2.JPG?format=1000w',
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-20812370/original/e6c6d885-0803-43fb-8431-e1a774afe565.jpeg?im_w=1200',
         preview: false,
       },
       {
         spotId: 3,
-        url: 'https://images.unsplash.com/photo-1625492995811-646c41068abe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmFubGlmZXxlbnwwfHwwfHw%3D&w=1000&q=80',
+        url: 'https://a0.muscache.com/im/pictures/4c8e96ed-8a7b-430c-9773-d56c09bb8c5d.jpg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 3,
+        url: 'https://a0.muscache.com/im/pictures/2df0354f-ac3e-4ae5-a4fd-22f5925ef544.jpg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 3,
+        url: 'https://a0.muscache.com/im/pictures/402be074-12c4-4a2c-914d-dc16430ae89f.jpg?im_w=1200',
+        preview: false,
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/396aa09f-1d52-434c-83dc-7a5390dd5633.jpg?im_w=1200',
+        preview: true,
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/1e292c98-0a71-4323-abbf-972397812c29.jpg?im_w=1200',
+        preview: false,
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/e66dfca9-8154-4c84-aa4a-d3b3fa645848.jpg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/79729b15-816f-4e88-af61-d36764116832.jpg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 4,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-53575727/original/1310e233-0cd1-45e5-8ac9-dff154ebd567.jpeg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 5,
+        url: 'https://a0.muscache.com/im/pictures/7aaab327-951e-43b5-aa78-07c197381bcd.jpg?im_w=1200',
+        preview: true,
+      },
+      {
+        spotId: 5,
+        url: 'https://a0.muscache.com/im/pictures/d15f941a-50d3-4154-b6bd-d57e4461d9bc.jpg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 5,
+        url: 'https://a0.muscache.com/im/pictures/e32e3afa-a853-4d45-a89f-2d92e1de7a12.jpg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 5,
+        url: 'https://a0.muscache.com/im/pictures/691deafa-7af1-414d-bb46-0b71a236f029.jpg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 5,
+        url: 'https://a0.muscache.com/im/pictures/4f2b43b4-1968-4575-b157-5e0e5be0d1f5.jpg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 6,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-48702041/original/005846e6-7f52-4ec9-b0f3-d99b8c25f861.png?im_w=1200',
+        preview: true,
+      },
+      {
+        spotId: 6,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-48702041/original/2b57e07c-2316-4cd2-8102-23615a51bdc3.png?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 6,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-48702041/original/eaa366e5-4386-475a-8b81-3e18410ee61e.png?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 6,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-48702041/original/d22e6f2d-23dc-4c56-82fb-5e9e49c742b0.png?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 6,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-48702041/original/e0ce1b07-9f82-4b9d-8191-95be5e22f90d.png?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 7,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-665186824025123182/original/e6f4e6a4-c3fa-4de0-85e7-6d35eebde745.jpeg?im_w=1200',
+        preview: true,
+      },
+      {
+        spotId: 7,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-665186824025123182/original/04fe3ed6-a321-4c1b-8d15-92e5fbb6a278.jpeg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 7,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-665186824025123182/original/fd85ef1b-a1ba-4157-b739-dd78885ddaaf.jpeg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 7,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-665186824025123182/original/40ce29bf-6c45-4fa7-bc7f-a4f5cb10dea3.jpeg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 7,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-665186824025123182/original/f1bb552b-6d41-4eab-9ca3-90e757be20c6.jpeg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 8,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-49130510/original/9a3bf715-ecd2-41b1-a5b1-6c354f371cb7.jpeg?im_w=1200',
+        preview: true,
+      },
+      {
+        spotId: 8,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-49130510/original/f1c0b03f-7249-42c5-a1f7-81fadcfe77cc.jpeg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 8,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-49130510/original/fc161e57-e38e-4e85-9b98-62061b3d552b.jpeg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 8,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-49130510/original/af951d0d-9ea5-4f09-b09b-9d09f5d9cbc2.jpeg?im_w=720',
+        preview: false,
+      },
+      {
+        spotId: 8,
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-49130510/original/209229b3-566d-4c0c-a70e-ebf28a1a0b9b.jpeg?im_w=720',
         preview: false,
       }
     ], {})
