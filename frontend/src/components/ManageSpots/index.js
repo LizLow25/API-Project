fcdfc24fff02b2a515ac6848682c72baefe20c62
Elizabeth Lowry <eglowry@gmail.com>
2023-07-23 15:53:05 -0600

    added footer

--- frontend/src/components/ManageSpots/index.js
+++ frontend/src/components/ManageSpots/index.js
@@ -1,6 +1,6 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { getSpotsByOwner } from "../../store/spots";
 import SpotCard from '../SpotCard'
 import './ManageSpots.css'
@@ -11,12 +11,18 @@ import DeleteSpotModal from '../DeleteSpotModal';
 import { getSpotBookingsThunk } from '../../store/bookings';
 
 const ManageSpots = () => {
+    const history = useHistory();
     const dispatch = useDispatch();
+    const [number , setNumber] = useState(15)
+
+
+
     const spotsData = useSelector(state => state.spots.allSpots);
 
     const user = useSelector(state => state.session.user)
 
     const spots = Object.values(spotsData);
+    const price = number * 181
 
     useEffect(() => {
         dispatch(getSpotsByOwner())
@@ -25,12 +31,16 @@ const ManageSpots = () => {
 
     if (user && !spots?.length) {
         return (
-            <div>
-                <h2>AirRV it.</h2>
-                <h3>You could earn</h3>
+            <div className='nospotsyet'>
+                <div className='airRvtitle'>AirRV it.</div>
+                <div className='earndiv'>You could earn</div>
+                <div className='earndiv'>$ {price.toLocaleString("en-US")} </div>
+                <div className='earnbynights'>{number} nights at at estimated $181 night</div>
                 <div class="slidecontainer">
-                    <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
+                    <input type="range" min="1" max="30" value={number} className="slider" onChange={(e) => setNumber(e.target.value)}/>
                 </div>
+
+                <button className='managebutton' onClick={() => history.push('/spots/new')}>Get started</button>
 
             </div>
         )
--- frontend/src/components/SpotList/index.js
+++ frontend/src/components/SpotList/index.js
@@ -16,25 +16,33 @@ const SpotList = () => {
     }, [dispatch], spotsData)
 
     return (
+        <>
         <div className='landingpage'>
 
 
-                {spots.map((spot) => (
-                    <div key={spot.id}>
-                        <Link to={`/spots/${spot.id}`} className='spotlink'
-                        >
-                            {console.log('spot.price', spot.price)}
-                            <SpotCard
-                                spot={spot}
-                                spotPrice={spot.price}
-                            />
-                        </Link>
-                    </div>
-                ))}
+            {spots.map((spot) => (
+                <div key={spot.id}>
+                    <Link to={`/spots/${spot.id}`} className='spotlink'
+                    >
+                        {console.log('spot.price', spot.price)}
+                        <SpotCard
+                            spot={spot}
+                            spotPrice={spot.price}
+                        />
+                    </Link>
+                </div>
+            ))}
 
-            
+
         </div>
-
+        <div className='person-container'>
+                <p>Created by Liz Lowry</p>
+                <div className='link-container'>
+                    <a target='_blank' href='https://www.linkedin.com/in/elizabeth-lowry-33201a14'><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+                    <a target='_blank' href='https://github.com/LizLow25'><i class="fa-brands fa-square-github fa-2xl"></i></a>
+                </div>
+            </div>
+</>
     )
 
 
